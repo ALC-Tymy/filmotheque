f7d8489a412210b04b007fa252cc70686ce9e616
Tymy <tymy@2a01cb06d023e9fe9e8038e9518212b7.ipv6.abo.wanadoo.fr>
2026-01-12 17:42:39 +0100

    Initial commit

---
+++ src/main/java/fr/eni/filmotheque/bll/FilmService.java
@@ -0,0 +1,25 @@
+package fr.eni.filmotheque.bll;
+
+import fr.eni.filmotheque.bo.Film;
+import fr.eni.filmotheque.bo.Genre;
+import fr.eni.filmotheque.bo.Participant;
+
+import java.util.List;
+
+public interface FilmService {
+    List<Film> consulterFilms();
+
+    Film consulterFilmParId(long id);
+
+    List<Genre> consulterGenres();
+
+    List<Participant> consulterParticipants();
+
+    Genre consulterGenreParId(long id);
+
+    Participant consulterParticipantParId(long id);
+
+    void creerFilm(Film film);
+
+    void deleteFilm(Film film);
+}
